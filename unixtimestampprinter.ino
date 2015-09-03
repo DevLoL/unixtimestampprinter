33f5256cc1fc17c6371f78d2d200c89f4564d4da
Thomas R. Koll <tomk32@tomk32.de>
2015-09-03 21:04:43 +0200

    First iteration of printer controller

--- unixtimestampprinter.ino
+++ unixtimestampprinter.ino
@@ -4,7 +4,13 @@
 
 /*
  * The magnificent Unix Timestamp Printer will print unique timestamps for your private use.
+ *
+ * Inspration and further readings:
+ *   http://elektronik-kompendium.de/public/arnerossius/bastel/m31a.htm
+ *   how to control a small Epson printer from a table calc with an AVR µC
+ *
  */
+
 
 // pins for the printer
 const int printer_magnet = 3;
@@ -12,11 +18,18 @@ const int printer_motor = 4;
 const int printer_char_start = 5;
 const int printer_char_end = 6;
 const int printer_char_reset = 7;
+static int printer_position;
 
-// print button, debounced
+const char printer_chars[] = "#123456789-,.";
+// second row is " +x:#*ENMC= -%"
+
+// print button
 const int print_timestamp = 8;
-const int time_since_print = 0;
+static int time_since_print = 0;
 const int time_between_prints = 5000; // milliseconds
+
+// timekeeping
+static int seconds = 1441121804; // roughly
 
 void setup() {
   pinMode(printer_magnet, OUTPUT);
@@ -28,17 +41,80 @@ void setup() {
 
 }
 
+// map a string into an array of positions on the printer wheel
+int *printerPositions(char str[]) {
+  int len = strlen(str);
+  int result[len];
+  for(int i=0; i < len; i++) {
+    result[i] = strchr(printer_chars, str[i]) - printer_chars;
+  }
+  return result;
+}
+
 void printTimeStamp() {
   // return if pressed recently
-  if(time_since_print > millis()) {
+  if (time_since_print > millis()) {
     return;
   }
   time_since_print = millis() + time_between_prints;
+  char str_seconds[18];
+  sprintf(str_seconds, "%d", seconds);
+  int *string_positions;
+  string_positions = printerPositions(str_seconds);
+
+  digitalWrite(printer_motor, HIGH);
+  static int printer_position = 0;
+
+  printerSeekStart();
+  // print from left to right
+
+  for(int i = sizeof(string_positions); i > 0; i--) {
+    printChar(string_positions[i]);
+  }
+
+  // for a line break we need to wait for a little longer before we release the magnet
+  printerSeekNextChar();
+  printerSeekNextChar();
+  digitalWrite(printer_magnet, LOW);
+
+  digitalWrite(printer_motor, LOW);
+}
+
+void printerSeekStart() {
+  if (digitalRead(printer_char_reset) == LOW) {
+    // bring the printer motor to 0 position
+    delay(5);
+  }
+}
+
+void printerSeekNextChar() {
+  if (printer_char_reset == HIGH) {
+    printer_position = 0;
+    return;
+  }
+  if (printer_char_start == HIGH) {
+    printer_position += 1;
+    return;
+  }
+  delay(5);
+}
+
+void printChar(int position) {
+  digitalWrite(printer_magnet, LOW);
+  do {
+    printerSeekNextChar();
+  } while(position != printer_position);
+
+  // actually print the char
+  do {
+    digitalWrite(printer_magnet, HIGH);
+    delay(5);
+  } while(printer_char_end == LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(digitalRead(print_timestamp) == HIGH) {
+  if (digitalRead(print_timestamp) == HIGH) {
     printTimeStamp();
   }
 }
